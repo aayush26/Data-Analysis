fa6dbdb6fe363a87fb41bcf9eac371fbfd467368
Aayush Kumar Singha <aayushsingh.mail@gmail.com>
2015-07-12 03:36:24 +0530

    kNN Clustering using R

---
+++ Clustering and Classification/Beginner/kNN using R/iris.r
@@ -0,0 +1,16 @@
+#load iris.csv
+#set the directory where the files are pulled
+setwd("C:/Users/The Timekeeper/Desktop/")
+#RStudio has default iris dataset. To use that, skip the below line.
+iris <- read.csv("iris.csv")            
+#all work are done on a copy variable of iris
+iris.copy <- iris
+#To display iris dataset, type View(iris)
+iris.copy$Species <- NULL
+#To display iris.copy dataset, type View(iris.copy)
+#Perform kNN clustering with arg(filename, no. of clusters)
+result = kmeans(iris.copy, 3)
+plot(iris[c("Petal.Length","Petal.Width")], col=result$cluster)
+#Uncomment the below line to check the original clustering
+#plot(iris[c("Petal.Length","Petal.Width")], col=iris$Species)
+
